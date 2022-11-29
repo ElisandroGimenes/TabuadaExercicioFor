7610f25f00c8c3c96e65dd48de77c13143334ce2
Elisandro <Elisandro@LAPTOP-CCH60LK6>
2022-11-29 00:33:45 -0300

    Adicionar arquivos de projeto.

---
+++ TabuadaExercicioFor/Program.cs
@@ -0,0 +1,16 @@
+﻿class Program
+{
+    public static void Main(String[] args)
+    {
+        int numero;
+
+        Console.Write("Digite o número da tabuada: ");
+        
+        numero = int.Parse(Console.ReadLine());
+        
+        for (int i = 0; i <= 10; i++)
+        {
+            Console.WriteLine(numero + " X " + i + " = " + numero * i);
+        }
+    }
+}
